82caf0ce64c31a17957f85d6cf7cd8a601ab83e1
spain <spain@LAPTOP-1EQ8AMBK>
2024-10-02 16:35:43 +0200

    "esta bien hecho"

---
+++ sucio/src/Interfaz/Quinto_ejercicio_bien.java
@@ -0,0 +1,10 @@
+package Interfaz;
+
+public class Quitno_e {
+	public static void main (String[]args) {
+		int radio = 2;
+		double pi = Math.PI;
+		int perimetro = (int) (2 * Math.PI * radio);
+		System.out.println("El perimetro del radio es " + perimetro);
+	}
+}
