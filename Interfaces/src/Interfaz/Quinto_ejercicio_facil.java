486c929bc59be8f1240064b4aadbd8d774d6129b
Yesa920 <izanabadmarina@gmail.com>
2024-10-02 16:42:08 +0200

    Quinto_ejercicio_facil.java

--- Interfaces/src/Interfaz/Quinto_ejercicio_facil.java
+++ Interfaces/src/Interfaz/Quinto_ejercicio_facil.java
@@ -3,8 +3,8 @@ package Interfaz;
 public class Quinto_ejercicio_facil {
 	public static void main (String [] args) {
 		int radio = 2;
-		Object math;
-		int perimetro = 2 * math.pi * radio;
+		double pi = Math.PI;
+		int perimetro = (int) (2 * Math.PI * radio);
 		System.out.println("El perimetro del radio es " + perimetro);
 	}
 }
