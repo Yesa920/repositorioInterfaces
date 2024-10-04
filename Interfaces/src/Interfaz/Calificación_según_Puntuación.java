7995ddc2174f3b25ef6d1aaaeff2fed27ed978d9
Yesa920 <izanabadmarina@gmail.com>
2024-10-04 12:14:08 +0200

    Calificación_según_Puntuación.java

--- Interfaces/src/Interfaz/Calificación_según_Puntuación.java
+++ Interfaces/src/Interfaz/Calificación_según_Puntuación.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class Calificación_según_Puntuación {
 	public static void main (String[]args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Dime tu nota y el programa te dice que letra has sacado");
+		System.out.println("Dime tu nota y el programa te dice que letra has sacado");
 		String variable = scanner.nextLine();
 		
 		int numero = Integer.parseInt(variable);
 		
 		if(numero > 100) {
-			System.out.print("Esa nota no existe escribela nuevamente");
+			System.out.println("Esa nota no existe escribela nuevamente");
 		}
 		else if (numero  >= 90) {
-			System.out.print("Tu puntuación es A");
+			System.out.println("Tu puntuación es A");
 		}
 		else if (numero  >= 80) {
-			System.out.print("Tu puntuación es B");
+			System.out.println("Tu puntuación es B");
 		}
 		else if (numero  >= 70) {
-			System.out.print("Tu puntuación es C");
+			System.out.println("Tu puntuación es C");
 		}
 		else if (numero  >= 60) {
-			System.out.print("Tu puntuación es D");
+			System.out.println("Tu puntuación es D");
 		}
 		else{
-			System.out.print("Tu puntuación es F");
+			System.out.println("Tu puntuación es F");
 		}
 	}
 	
