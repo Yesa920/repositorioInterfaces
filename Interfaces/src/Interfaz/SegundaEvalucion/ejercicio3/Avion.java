b6818f417f8ab65f1b19fd2c102a283e6bd74f50
Yesa920 <izanabadmarina@gmail.com>
2024-12-09 18:31:41 +0100

    Add files via upload

---
+++ Interfaces/src/Interfaz/SegundaEvalucion/ejercicio3/Avion.java
@@ -0,0 +1,14 @@
+package ejercicio_3;
+
+public class Avion extends Vehiculo{
+	private boolean volar;
+	public boolean getVolar() {
+		return volar;
+	}
+	public void setVolar(boolean vol) {
+		this.volar = vol;
+	}
+	public Avion() {
+		super();
+	}
+}
---
+++ Interfaces/src/Interfaz/SegundaEvalucion/ejercicio3/Coche.java
@@ -0,0 +1,8 @@
+package ejercicio_3;
+
+public class Coche extends Vehiculo{
+	public Coche() {
+		super();
+	}
+	
+}
