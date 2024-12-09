4800d3c51b405e9d0645ad9bbfb7e9ab58cf23d8
Yesa920 <izanabadmarina@gmail.com>
2024-12-09 18:32:51 +0100

    Add files via upload

---
+++ Interfaces/src/Interfaz/SegundaEvalucion/ejercicio4/Empleado.java
@@ -0,0 +1,24 @@
+package ejercicio4;
+
+public class Empleado {
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nom) {
+		this.nombre = nom;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double sal) {
+		this.salario = sal;
+	}
+	private double salario;
+	
+	
+	
+	public Empleado() {
+		
+	}
+}
---
+++ Interfaces/src/Interfaz/SegundaEvalucion/ejercicio4/Gerente.java
@@ -0,0 +1,9 @@
+package ejercicio4;
+
+public class Gerente extends Empleado{
+	public Gerente() {
+		super();
+		setSalario(89);
+		setNombre("Lider");
+	}
+}
