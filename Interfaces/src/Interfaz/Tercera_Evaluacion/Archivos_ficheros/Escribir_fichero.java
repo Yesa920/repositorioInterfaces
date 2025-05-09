bdd898d699412b0dd5edcda14d49e0de781f6022
Yesa920 <izanabadmarina@gmail.com>
2025-05-09 13:02:18 +0200

    Update Escribir_fichero.java

--- Interfaces/src/Interfaz/Tercera_Evaluacion/Archivos_ficheros/Escribir_fichero.java
+++ Interfaces/src/Interfaz/Tercera_Evaluacion/Archivos_ficheros/Escribir_fichero.java
@@ -7,15 +7,19 @@ import java.io.IOException;
 public class Escribir_fichero {
 
 	public static void main(String[] args) {
-		String ruta = "entrada.txt";
 
-		try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
-			String linea;
-			while ((linea = br.readLine()) != null) {
-				System.out.println(linea);
+		String[] nombres = { "Izan", "Luis", "Skiper", "PrimiRey" };
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("nombres.txt"))) {
+			for (String nombre : nombres) {
+				bw.write(nombre);
+				bw.newLine();
+
 			}
+			System.out.println("La informacion ha sido escrita exitosamente");
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Error");
 		}
 	}
 
